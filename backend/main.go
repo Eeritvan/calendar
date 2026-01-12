96a05da07e9db6a7aefea2e6d053dbe8dd4d68a2
eeritvan <eeritvan@proton.me>
2026-01-12 23:34:08 +0200

    feat(backend): added initial endpoint for websocket

--- backend/main.go
+++ backend/main.go
@@ -5,18 +5,49 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/eeritvan/calendar/internal/api"
 	"github.com/eeritvan/calendar/internal/sqlc"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
-
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+func wsHandler(c echo.Context) error {
+	conn, err := websocket.Accept(c.Response(), c.Request(), &websocket.AcceptOptions{
+		InsecureSkipVerify: true, // TODO
+	})
+	if err != nil {
+		return err
+	}
+	defer conn.CloseNow()
+
+	ctx := c.Request().Context()
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err = wsjson.Write(ctx, conn, "testing")
+			if err != nil {
+				// TODO: error handling
+				fmt.Println(err)
+				return err
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
 
 func main() {
 	if err := godotenv.Load(".env.local"); err != nil {
@@ -57,13 +88,14 @@ func main() {
 		},
 		Skipper: func(c echo.Context) bool {
 			switch c.Path() {
-			case "/api/signup", "/api/login", "/api/totp/authenticate", "/api/totp/recovery":
+			case "/api/signup", "/api/login", "/api/totp/authenticate", "/api/totp/recovery", "/ws":
 				return true
 			}
 			return false
 		},
 	}))
 
+	e.GET("/ws", wsHandler)
 	api.RegisterHandlers(basePath, server)
 
 	port := os.Getenv("PORT")
