47c945256d3fb854a2214dea46ad4da2c56787f4
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-03-18 12:37:14 +0200

    chore: updated deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,11 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    devtools(),
+    devtools({
+      consolePiping: {
+        enabled: false
+      }
+    }),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
